36b2f4b86ad4891388854e742da5c5a9014d304a
izaelbm <46963796+izaelbm@users.noreply.github.com>
2021-10-11 20:00:35 -0300

    Create FileUpload.php

---
+++ FileUpload.php
@@ -0,0 +1,69 @@
+<html>
+
+	<title>File Upload</title>
+
+	<head>File Upload</head>
+
+	<body>
+
+		<form action="fileUpload.php7" method="post" enctype="multipart/form-data">
+
+			<label>Selecione o Arquivo</label>
+
+			<input type="file" id="file" name="file"/>
+
+			<input type="submit" value="Enviar" name="submit">
+
+		</form>
+
+	</body>
+
+</html>
+
+
+
+<?php
+
+
+
+if(isset($_POST['submit'])){
+
+
+
+    $dir = "./";
+
+
+
+    $dirFile = $dir . basename($_FILES["file"]["name"]);
+
+
+
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $dirFile)) {
+
+
+
+        echo "O Arquivo ". htmlspecialchars( basename( $_FILES["file"]["name"])). " Foi enviado com sucesso. [ ".$dirFile." ]";
+
+
+
+      } else {
+
+
+
+        echo "Erro ao enviar arquivo.";
+
+
+
+      }
+
+}else{
+
+}
+
+
+
+
+
+
+
+?>
